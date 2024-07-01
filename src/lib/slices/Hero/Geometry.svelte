36993c270acfd6bc0da1e2142ef5d3e0f75a0855
GregM1991 <gregm31@live.com>
2024-07-02 04:31:53 +1000

    Added some geometry to the scene

---
+++ src/lib/slices/Hero/Geometry.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { T as Threlte } from '@threlte/core';
+	import { Float } from '@threlte/extras';
+	import * as THREE from 'three';
+
+	export let position: [number, number, number] = [0, 0, 0];
+	export let geometry: THREE.BufferGeometry = new THREE.IcosahedronGeometry(3);
+	export let rate = 0.5;
+
+	const material = new THREE.MeshStandardMaterial({
+		color: 'tomato',
+		roughness: 0.1,
+		metalness: 0.5
+	});
+</script>
+
+<Threlte.Group position={position.map((p) => p * 2)}>
+	<Float
+		speed={5 * rate}
+		rotationSpeed={5 * rate}
+		rotationIntensity={6 * rate}
+		floatIntensity={5 * rate}
+	>
+		<Threlte.Mesh {geometry} {material}></Threlte.Mesh>
+	</Float>
+</Threlte.Group>
---
+++ src/lib/slices/Hero/Shapes.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { T as Threlte } from '@threlte/core';
+	import * as THREE from 'three';
+	import Geometry from './Geometry.svelte';
+</script>
+
+<Threlte.PerspectiveCamera
+	makeDefault
+	position={[0, 0, 25]}
+	aspectRatio={1}
+	fieldOfVision={30}
+	near={1}
+	far={40}
+/>
+
+<Geometry />
+
+<Threlte.DirectionalLight intensity={2} postition{[0 ,10 ,30]}></Threlte.DirectionalLight>
