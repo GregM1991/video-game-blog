53b6c4e0aef35ec0af03916179056e12c050a310
Greg <gregm31@live.com>
2024-07-09 03:50:37 +1000

    WIP

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -11,6 +11,8 @@
 				href="/"
 				class="text-xl font-extrabold tracking-tighter text-slate-100 transition-colors duration-150 hover:text-yellow-400"
 			>{settings.data.name}</a>
+			<span class="hidden text-5xl font-extralight leading-[0] text-slate-400 sm:inline">/</span>
+			
 		</div>
 		<div class="navigation">Profile</div>
 		<div class="socials">Tiwtter</div>
